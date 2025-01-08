8a54acea6596fc74639950aacd62c1bcfd6865c5
vietcao66 <161188885+vietcao66@users.noreply.github.com>
2025-01-08 14:25:30 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log('Hello World!');
+let name = 'Alice'; // String
+const age = 25;     // Number
+let isStudent = true; // Boolean
+let hobbies = ['reading', 'coding', 'gaming']; // Array
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(hobbies);
