805559d079e9780c304f61c2ec50c043e974deb8
vietcao66 <161188885+vietcao66@users.noreply.github.com>
2025-01-10 18:12:26 +0700

    1 ngay toi te

--- src/server.js
+++ src/server.js
@@ -1,20 +1,25 @@
 const express = require("express");
-const path = require("path"); //commo js module
+const path = require("path"); //common js module
+require("dotenv").config();
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 3000;
+const hostname = process.env.HOST_NAME;
+
+//config static file
+app.use(express.static(path.join(__dirname, "public")));
 
 //config template engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Day la trang chu");
 });
 
 app.get("/abc", (req, res) => {
   res.render("sample.ejs");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
